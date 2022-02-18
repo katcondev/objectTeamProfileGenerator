e84c907f3df05eed23e0dc5410d7229486378ffa
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-02-18 12:58:24 -0800

    updatin prompt strategy

--- index.js
+++ index.js
@@ -1,97 +1,73 @@
-const inquirer = require('inquirer');
-const generatePage = require('./src/page-template.js');
-const { writeFile, copyFile } = require('./src/generate-site.js');
+const inquirer = require("inquirer");
+const generateTeamPage = require("./src/page-template.js");
+const { writeFile, copyFile } = require("./src/generate-site.js");
 
 const Manager = require("./lib/manager.js");
 const Engineer = require("./lib/engineer.js");
-const Intern = require("./lib/intern.js")
+const Intern = require("./lib/intern.js");
 
 const employees = [];
 
 const addTeamMember = async () => {
-    inquirer.prompt([{
+  return inquirer
+    .prompt([
+      {
         message: "Enter team member's name",
-        name: "name"
-    },
-    {
+        name: "name",
+      },
+      {
         type: "list",
         message: "Select team member's role",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager"
-        ],
-        name: "role"
-    },
-    {
+        choices: ["Engineer", "Intern", "Manager"],
+        name: "role",
+      },
+      {
         message: "Enter team member's id",
-        name: "id"
-    },
-    {
+        name: "id",
+      },
+      {
         message: "Enter team member's email address",
-        name: "email"
-    }])
-    .then(function({name, role, id, email}) {
-        let roleInfo = "";
-        if (role === "Engineer") {
-            roleInfo = "GitHub username";
-        } else if (role === "Intern") {
-            roleInfo = "school name";
-        } else {
-            roleInfo = "office phone number";
-        }
-        inquirer.prompt([{
-            message: `Enter team member's ${roleInfo}`,
-            name: "roleInfo"
-        },
-        {
-            type: "list",
-            message: "Would you like to add more team members?",
-            choices: [
-                "yes",
-                "no"
-            ],
-            name: "moreMembers"
-        }])
-        .then(function({roleInfo, moreMembers}) {
-            let newMember;
-            if (role === "Engineer") {
-                newMember = new Engineer(name, id, email, roleInfo);
-            } else if (role === "Intern") {
-                newMember = new Intern(name, id, email, roleInfo);
-            } else {
-                newMember = new Manager(name, id, email, roleInfo);
-            }
-            employees.push(newMember);
-            addHtml(newMember)
-            .then(function() {
-                if (moreMembers === "yes") {
-                    addTeamMember();
-                } else {
-                    generatePage();
-                }
-            });
-        
-        });
-    });
-}
- 
-
-addTeamMember()
-  .then(newMember)
-  .then(employees => {
-      return generatePage(employees);
-  })
-  .then(pageHTML => {
+        name: "email",
+      },
+      {
+        message: `Enter team member's ${roleInfo}`,
+        name: "roleInfo",
+      },
+      {
+        type: "confirm",
+        name: "moreMembers",
+        message: "Would you like to add more team members?",
+        default: false
+      }
+    ])
+    .then(function ({ name, role, id, email }) {
+      let roleInfo = "";
+      if (role === "Engineer") {
+        roleInfo = "GitHub username";
+      } else if (role === "Intern") {
+        roleInfo = "School name";
+      } else {
+        roleInfo = "Office phone number";
+      }
+    })
+    .then(newMember => {
+      employees.push(newMember);
+      if (moreMembers === "yes") {
+        addTeamMember();
+      } else {
+       return generateTeamPage(newMember);
+      }
+    })
+    .then(pageHTML => {
       return writeFile(pageHTML);
-  })
-  .then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-  })
-//   .then(copyFileResponse => {
-//     console.log(copyFileResponse);
-//   })
-  .catch(err => {
-    console.log(err);
-  });
+    })
+    .then(writeFileResponse => {
+      console.log(writeFileResponse);
+      return copyFile();
+    })
+    .then(copyFileResponse => {
+      console.log(copyFileResponse);
+    })
+    .catch(err => {
+      console.log(err);
+    })
